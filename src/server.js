c2df75d1c50de9f54cd55d9dda51ef6c40bba863
KyzmGD <tuanlinhosu206@gmail.com>
2026-07-21 20:39:06 +0700

    1

--- src/server.js
+++ src/server.js
@@ -6,7 +6,13 @@ const cors = require("cors");
 const app = express();
 
 
-app.use(cors());
+app.use(
+  cors({
+    origin:
+      "https://decor-frontend-pi.vercel.app/",
+    credentials: true
+  })
+);
 app.use(express.json());
 const sequelize =
   require("./config/db");
